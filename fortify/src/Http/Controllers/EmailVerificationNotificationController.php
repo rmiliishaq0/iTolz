2f49b02d30c1044f810d2db44b74aad2efb25817
rmiliishaq0 <=alferdmony41@gmail.com>
2025-08-11 01:11:12 +0100

    Add built assets for deployment

---
+++ fortify/src/Http/Controllers/EmailVerificationNotificationController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Laravel\Fortify\Http\Controllers;
+
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Laravel\Fortify\Contracts\EmailVerificationNotificationSentResponse;
+use Laravel\Fortify\Http\Responses\RedirectAsIntended;
+
+class EmailVerificationNotificationController extends Controller
+{
+    /**
+     * Send a new email verification notification.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $v = $request->validate(['g-recaptcha-response' => 'required|captcha']);
+        if ($request->user()->hasVerifiedEmail()) {
+            return $request->wantsJson()
+                        ? new JsonResponse('', 204)
+                        : app(RedirectAsIntended::class, ['name' => 'email-verification']);
+        }
+
+        $request->user()->sendEmailVerificationNotification();
+
+        return app(EmailVerificationNotificationSentResponse::class);
+    }
+}
